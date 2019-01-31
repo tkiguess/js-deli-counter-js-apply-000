2851d2ad817b3b849cb325e06f336dcb301f174a
Tafkir Ahmed <ahmedtafkir@gmail.com>
2019-01-31 16:19:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ function takeANumber(line, name){
   return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
 
+function nowServing(line) {
+  if(!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    return "Currently serving " + line.shift() + ".";
+  }
+}
+
 function currentLine(line){
   if(!line.length) {
     return "The line is currently empty.";
@@ -20,13 +29,4 @@ function currentLine(line){
   }
   console.log("The line is currently: " + lineVariables)
   return "The line is currently: " + lineVariables.join(', ');
-}
-
-function nowServing(line) {
-  if(!line.length) {
-    console.log("There is nobody waiting to be served!")
-    return "There is nobody waiting to be served!"
-  } else {
-    return "Currently serving " + line.shift() + ".";
-  }
 }
