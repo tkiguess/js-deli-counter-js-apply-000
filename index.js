32766dae83cc9b6b33a25d8e934ddde4d09d532c
Tafkir Ahmed <ahmedtafkir@gmail.com>
2019-01-31 16:00:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var katzDeli = [];
 
+
+function takeANumber(line, name){
+  line.push(name);
+
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
+
 function currentLine(line){
   if(!line.length) {
     return "The line is currently empty.";
@@ -22,10 +31,3 @@ function nowServing(line) {
   }
 }
 
-function takeANumber(line, name){
-  line.push(name);
-
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
-
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
-}
